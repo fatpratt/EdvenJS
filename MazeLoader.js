7a8e8523c0f1726934505d70919841926050ca4f
Brian Pratt <brian.pratt@pearson.com>
2013-11-01 21:58:33 -0600

    Refactored MazeLoader code to use callbacks to combat indent fatigue in loadAndStart() method.

--- MazeLoader.js
+++ MazeLoader.js
@@ -7,6 +7,35 @@
 // Namespace: MazeLoader
 if (MazeLoader == null || typeof(MazeLoader) != "object") {var MazeLoader = new Object();}
 
+MazeLoader.maze = null;
+MazeLoader.mapData = null;
+MazeLoader.propData = null;
+MazeLoader.mazeConfig = null;
+MazeLoader.textAreaBox = null;
+MazeLoader.ctx = null;
+
+// CallBack
+var propDataLoadAssociatedImagesCallBack = function(statusGood2, message2) {
+    if (statusGood2) {
+        console.log('MazeLoader.js: successfully loaded associated prop image files');
+        MazeLoader.maze = new Maze(MazeLoader.ctx, MazeLoader.mapData, MazeLoader.propData, MazeLoader.mazeConfig);       // happy path
+        MazeLoader.maze.renderOneFrame();
+    } else {
+        MazeLoader.textAreaBox.dumpError('error unable to load associated images: ' + message2);
+        return;
+    }
+};
+
+// CallBack
+var loadDataFileCallBack = function(statusGood, message) {
+    if (statusGood) console.log('MazeLoader.js: Prop Data file successfully loaded.');
+    else {
+        MazeLoader.textAreaBox.dumpError('error unable to load Prop Data file: ' + message);
+        return;
+    }
+    MazeLoader.propData.loadAssociatedImages(propDataLoadAssociatedImagesCallBack);
+};
+
 //----------------------------------------------------------------------------------------------------------------------
 // Constructor
 //----------------------------------------------------------------------------------------------------------------------
@@ -36,45 +65,30 @@ MazeLoader.prototype.createCanvas = function() {
     return canvas;
 };
 
+
 //--------------------------------------------------------------------------------------------------
 // Loads map files, config files and starts things up.
 //--------------------------------------------------------------------------------------------------
-MazeLoader.prototype.loadAndStart = function(ctx, mazeId, textAreaBox) {
-    var mapData = new MapData(document, mazeId, textAreaBox);
-    mapData.loadDataFile(function(statusGood, message) {
+MazeLoader.prototype.loadAndStart = function(mazeId) {
+    MazeLoader.mapData = new MapData(document, mazeId, MazeLoader.textAreaBox);
+    MazeLoader.mapData.loadDataFile(function(statusGood, message) {
         if (statusGood) console.log('Maze.html: Map Data file successfully loaded.');
         else {
-            textAreaBox.dumpError('error unable to load Map Data file: ' + message);
+            MazeLoader.textAreaBox.dumpError('error unable to load Map Data file: ' + message);
             return;
         }
-        mapData.loadAssociatedImages(function(statusGood2, message2) {
+        MazeLoader.mapData.loadAssociatedImages(function(statusGood2, message2) {
             if (statusGood2) console.log('Maze.html: successfully loaded associated wall image files');
             else {
-                textAreaBox.dumpError('error unable to load associated images: ' + message2);
+                MazeLoader.textAreaBox.dumpError('error unable to load associated images: ' + message2);
                 return;
             }
         });
 
-        var mazeConfig = new MazeConfig(document, mazeId, textAreaBox, mapData.mapHeight, mapData.mapWidth);
-        mazeConfig.loadConfigFile(function(){
-            var propData = new PropData(document, mazeId, textAreaBox, mapData.mapHeight, mapData.mapWidth, mapData.mapWidthShift);
-            propData.loadDataFile(function(statusGood, message) {
-                if (statusGood) console.log('Maze.html: Prop Data file successfully loaded.');
-                else {
-                    textAreaBox.dumpError('error unable to load Prop Data file: ' + message);
-                    return;
-                }
-                propData.loadAssociatedImages(function(statusGood2, message2) {
-                    if (statusGood2) {
-                        console.log('Maze.html: successfully loaded associated prop image files');
-                        maze = new Maze(ctx, mapData, propData, mazeConfig);       // happy path
-                        maze.renderOneFrame();
-                    } else {
-                        textAreaBox.dumpError('error unable to load associated images: ' + message2);
-                        return;
-                    }
-                });
-            });
+        MazeLoader.mazeConfig = new MazeConfig(document, mazeId, MazeLoader.textAreaBox, MazeLoader.mapData.mapHeight, MazeLoader.mapData.mapWidth);
+        MazeLoader.mazeConfig.loadConfigFile(function(){
+            MazeLoader.propData = new PropData(document, mazeId, MazeLoader.textAreaBox, MazeLoader.mapData.mapHeight, MazeLoader.mapData.mapWidth, MazeLoader.mapData.mapWidthShift);
+            MazeLoader.propData.loadDataFile(loadDataFileCallBack);
 
         });
     });
@@ -85,21 +99,21 @@ MazeLoader.prototype.loadAndStart = function(ctx, mazeId, textAreaBox) {
 //--------------------------------------------------------------------------------------------------
 MazeLoader.prototype.init = function() {
     var canvas = this.createCanvas();
-    var ctx = canvas.getContext('2d');
+    MazeLoader.ctx = canvas.getContext('2d');
 
     var mazeIdTextFld = document.getElementById("mazeId");
     var mazeId = mazeIdTextFld.value;
 
-    var textAreaBox = new TextAreaBox(ctx, "black", "#aaaaaa",
+    MazeLoader.textAreaBox = new TextAreaBox(MazeLoader.ctx, "black", "#aaaaaa",
         MazeGlobals.PROJECTIONPLANEWIDTH, MazeGlobals.TEXTBOXWIDTH, MazeGlobals.TEXTBOXHEIGHT);
-    textAreaBox.dumpText('test test test');
+    MazeLoader.textAreaBox.dumpText('test test test');
 
-    if (ctx.createImageData == null) {
-        textAreaBox.dumpError('Please upgrade your browser. Sorry, your browser is out of date for this viewer to function properly.');
+    if (MazeLoader.ctx.createImageData == null) {
+        MazeLoader.textAreaBox.dumpError('Please upgrade your browser. Sorry, your browser is out of date for this viewer to function properly.');
         return;
     }
 
-    this.loadAndStart(ctx, mazeId, textAreaBox);
+    this.loadAndStart(mazeId);
 
     intervalId = window.setInterval(this.render, 30);
     window.addEventListener('keydown', this.doKeyDown, false);
@@ -110,7 +124,7 @@ MazeLoader.prototype.init = function() {
 // On key down event.
 //--------------------------------------------------------------------------------------------------
 MazeLoader.prototype.doKeyDown = function(evt) {
-    if (maze == null) return;
+    if (MazeLoader.maze == null) return;
     switch (evt.keyCode) {
         case 38: // Up arrow was pressed
             this.upArrowEvent = true;
@@ -131,7 +145,7 @@ MazeLoader.prototype.doKeyDown = function(evt) {
 // On key up event.
 //--------------------------------------------------------------------------------------------------
 MazeLoader.prototype.doKeyUp = function(evt) {
-    if (maze == null) return;
+    if (MazeLoader.maze == null) return;
     switch (evt.keyCode) {
         case 38: // Up arrow was released
             this.upArrowEvent = false;
@@ -152,39 +166,39 @@ MazeLoader.prototype.doKeyUp = function(evt) {
 // Render a frame.
 //--------------------------------------------------------------------------------------------------
 MazeLoader.prototype.render = function() {
-    if (maze == null) return;
+    if (MazeLoader.maze == null) return;
 
     if (this.upArrowEvent) {
-        maze.moveForward();
-        maze.renderOneFrame();
+        MazeLoader.maze.moveForward();
+        MazeLoader.maze.renderOneFrame();
         this.upArrowEvent = false;
         this.courtesyCnt = 0;
     }
 
     if (this.downArrowEvent) {
-        maze.moveBackward();
-        maze.renderOneFrame();
+        MazeLoader.maze.moveBackward();
+        MazeLoader.maze.renderOneFrame();
         this.downArrowEvent = false;
         this.courtesyCnt = 0;
     }
 
     if (this.leftArrowEvent) {
-        maze.rotateLeft();
-        maze.renderOneFrame();
+        MazeLoader.maze.rotateLeft();
+        MazeLoader.maze.renderOneFrame();
         this.leftArrowEvent = false;
         this.courtesyCnt = 0;
     }
 
     if (this.rightArrowEvent) {
-        maze.rotateRight();
-        maze.renderOneFrame();
+        MazeLoader.maze.rotateRight();
+        MazeLoader.maze.renderOneFrame();
         this.rightArrowEvent = false;
         this.courtesyCnt = 0;
     }
 
     // every once in a while you need to do a courtesy render even if there are no events
     if (this.courtesyCnt == 15) {     // assumes this function is called on a regular interval
-        maze.renderOneFrame();
+        MazeLoader.maze.renderOneFrame();
         this.courtesyCnt = 0;
     }
     this.courtesyCnt++;
